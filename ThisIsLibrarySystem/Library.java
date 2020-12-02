8e86d2a3392e25c01be6aa9553565408b0f5e589
DominikK07 <65187196+DominikK07@users.noreply.github.com>
2020-12-02 22:01:39 +0100

    Check Fines method finish?

--- ThisIsLibrarySystem/Library.java
+++ ThisIsLibrarySystem/Library.java
@@ -6,13 +6,11 @@ public class Library {
     private static Book book2 = new Book(2, "Mein Kampf");
     private static Reader reader1 = new Reader("Joe", "joemamma@hotmail.com", 1);
     private static Reader reader2 = new Reader("Jane", "janeee@gmail.com", 2);
-    public static Date dateToday = new Date();
+    public static Date dateToday;
     private int fine;
 
-    public void setDateToday(int day, int month, int year) {
-        Library.dateToday.setDay(day);
-        Library.dateToday.setMonth(month);
-        Library.dateToday.setYear(year);
+    public void setDateToday(Date dateToday) {
+        Library.dateToday = dateToday;
     }
 
     public static Date getDateToday() {
@@ -20,21 +18,29 @@ public class Library {
     }
 
     public int checkFines(){
-        Book.dateExpected.getDay();
-        Book.dateExpected.getMonth();
-        Book.dateExpected.getYear();
 
-        Book.dateReturned.getDay();
-        Book.dateReturned.getMonth();
-        Book.dateReturned.getYear();
-        if (Book.dateReturned.getDay()<=Book.dateExpected.getDay() && Book.dateReturned.getMonth()<=Book.dateExpected.getMonth()) {
+        //Returning the book before the expected date
+        if (Book.dateReturned.getDay()<=Book.dateExpected.getDay() &&
+        Book.dateReturned.getMonth()<=Book.dateExpected.getMonth() &&
+        Book.dateReturned.getYear()==Book.dateExpected.getYear()) {
             fine=0;
-        }if (Book.dateReturned.getDay()==Book.dateExpected.getDay() && Book.dateReturned.getMonth()==Book.dateExpected.getMonth()) {
-            
+        }
+        //Returning the book after the expected return date, but within the same calendar month of the expected date
+        if (Book.dateReturned.getDay()>=Book.dateExpected.getDay() &&
+        Book.dateReturned.getMonth()==Book.dateExpected.getMonth() &&
+        Book.dateReturned.getYear()==Book.dateExpected.getYear()) {
+            fine=15*(Book.dateReturned.getDay()-Book.dateExpected.getDay());
+        }
+        //Returning the book after the expected return date and the calendar month, but within the same year of the expected date
+        if (Book.dateReturned.getMonth()>Book.dateExpected.getMonth() &&
+        Book.dateReturned.getYear()==Book.dateExpected.getYear()) {
+            fine=500*(Book.dateReturned.getMonth()-Book.dateExpected.getMonth());
+        }
+        else {
+            fine=10000;
         }
         return fine;
-    }
-
+}
 
     public static void main(String[] args) {
         Library library = new Library();
@@ -42,10 +48,9 @@ public class Library {
         
         book1.get(reader1);
 
-        System.out.println("Today's date is " + dateToday.toString());
-        System.out.println("\nReader 1: " + reader1.toString());
-        System.out.println("\n" + book1.toString());
-        System.out.println("\n" + book2.toString());
+        System.out.println(reader1.toString());
+        System.out.println(book1.toString());
+        System.out.println("Today's date is " + dateToday.getDay() + " " + dateToday.getMonth() + " " + dateToday.getYear());
 
     }
 }
