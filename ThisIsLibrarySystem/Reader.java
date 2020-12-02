69228ae5faa68fc4555895c37d1308e5d6409b86
DominikK07 <65187196+DominikK07@users.noreply.github.com>
2020-12-02 22:38:46 +0100

    Comments

--- ThisIsLibrarySystem/Reader.java
+++ ThisIsLibrarySystem/Reader.java
@@ -5,16 +5,21 @@ public class Reader {
     private String email;
     private int ID; // TODO: must be > 0
     
+
+    //Constructor of the Reader object
+    //Assigns the name, email and ID of the reader
     Reader(String name, String email, int ID) {
         this.name = name;
         this.email = email;
         this.ID = ID;
     }
 
+    //Getter method for the name of the reader
     public String getName() {
         return name;
     }
 
+    //toString method -> returns a string representation of the object
     @Override
     public String toString() {
         return "Name: " + name + "; ID: " + ID + "; Email: " + email;
