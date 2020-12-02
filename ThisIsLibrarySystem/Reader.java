4a5f9e60a1b0dc67ae7fd5cf16812f308011c6bc
mfmcl <michael.mclean@student.leaf.academy>
2020-12-02 20:25:27 +0100

    -

--- ThisIsLibrarySystem/Reader.java
+++ ThisIsLibrarySystem/Reader.java
@@ -5,11 +5,6 @@ public class Reader {
     private String email;
     private int ID; // TODO: must be > 0
     
-
-    Reader() {
-        
-    }
-
     Reader(String name, String email, int ID) {
         this.name = name;
         this.email = email;
